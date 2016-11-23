5aa980daad615a5afd191ca8ec19713872b208c1
iatienza <ibai.atienza@gmail.com>
2016-11-24 00:25:41 +0100

    Install Nodejs 6.x and last npm packages in order to use a supported version of hubot

--- provision.sh
+++ provision.sh
@@ -2,14 +2,18 @@
 
 apt-get -y update
 
+if ! grep -q "nodesource" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+fi
+
 dpkg -s npm &>/dev/null || {
     #If not installed then install it
-    apt-get -y install nodejs npm
-    ln -s /usr/bin/nodejs /usr/bin/node
+    apt-get -y install nodejs
+    #ln -s /usr/bin/nodejs /usr/bin/node -> No needed for version 6.x
 }
 
 command -v hubot &>/dev/null || {
     # Test if hubot command exists con shell, if not then install it from npm
-    #npm install -g yo generator-hubot
-    npm install -g hubot coffee-script
+    npm install -g yo generator-hubot
+    #npm install -g hubot coffee-script
 }
